3483429bac44504a49c1fb36f133a616f18cb370
Relise228 <andreysmit43@gmail.com>
2020-04-30 14:35:52 +0300

    LoginFunction

--- src/sample/ServerAction.java
+++ src/sample/ServerAction.java
@@ -1,4 +1,58 @@
 package sample;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+
 public class ServerAction {
+
+    byte serverAddress[] = {127, 0, 0, 1,};
+    int serverPort = 13564;
+    int buffersize = 3000;
+
+
+    public void logIn(String log, String pass) {
+
+        String msg = "login@" + log.trim() + "|" + pass.trim();
+
+        try {
+            do {
+                DatagramSocket aSocket = new DatagramSocket();
+                byte[] msgByte = msg.getBytes();
+
+                InetAddress serverInetAddress = InetAddress.getByAddress(serverAddress); // створення объекту за IP-адресою
+
+                DatagramPacket request = new DatagramPacket(msgByte, msg.length(), serverInetAddress, serverPort);
+                aSocket.send(request);        //надсилає пакет
+                byte[] buffer = new byte[buffersize];
+                DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
+
+                aSocket.receive(reply);
+
+                String repl = new String(reply.getData());
+
+                if (!repl.trim().equals("")) {
+                    System.out.println("ok" + repl.trim());
+                }
+
+                aSocket.close();
+                break;
+            } while (msg.trim().equals("quit"));
+
+            // помилка при створення socket
+        } catch (
+                SocketException e) {
+            System.out.println("(Client) Socket: " + e.getMessage());
+
+            // помилка при отриманні
+        } catch (
+                IOException e) {
+            System.out.println("(Client) IO: " + e.getMessage());
+        }
+    }
+
+
 }
