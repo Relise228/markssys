9eaec25e0f4c86510bd9b5cecfd6138c844672f9
cookie4is <63065558+cookie4is@users.noreply.github.com>
2020-04-30 13:53:47 +0300

    Added teacher panel with controller.

---
+++ src/sample/TeacherPanel.java
@@ -0,0 +1,37 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
+public class TeacherPanel {
+
+    @FXML
+    private Button reportButton;
+
+    @FXML
+    private Label teacherName;
+
+    @FXML
+    private Button castButton;
+
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private TableView resultTable;
+
+    @FXML
+    private TableView groupTable;
+
+    @FXML
+    private TableView courseTable;
+
+    @FXML
+    void initialize() {
+
+    }
+}
