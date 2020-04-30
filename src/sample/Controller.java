798453a2e77c7d26ceb3209683a0dea20cc5676b
Relise228 <andreysmit43@gmail.com>
2020-04-30 13:04:47 +0300

    LogIn Window

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,31 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private TextField usrNameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    void initialize() {
+
+    }
 }
