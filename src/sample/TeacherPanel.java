c6520e2242ccec129bf12b15d67d280e31127952
cookie4is <63065558+cookie4is@users.noreply.github.com>
2020-04-30 13:55:57 +0300

    Added teacher panel with controller, stub for admin panel.

--- src/sample/TeacherPanel.java
+++ src/sample/TeacherPanel.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 
 public class TeacherPanel {
@@ -29,6 +30,12 @@ public class TeacherPanel {
 
     @FXML
     private TableView courseTable;
+
+    @FXML
+    private TableColumn course;
+
+    @FXML
+    private TableColumn group;
 
     @FXML
     void initialize() {
